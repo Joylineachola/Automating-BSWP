5fd8e67815e589dd38483d154d5b373510401cc7
Joylineachola <joybianca255@gmail.com>
2024-02-27 09:46:47 +0300

    debugging

---
+++ introduction/firstprogram.py
@@ -0,0 +1,11 @@
+# This program asks your name and your age and claculates next years age
+
+print("Hello there!, What is your name")
+myName = input()
+print(f"How old are you,{myName }")
+myAge = int(input())
+print(f'You will be {myAge +1} years next year')
+
+
+#the len function
+len('My very energetic monster just scarfed nachos.')
